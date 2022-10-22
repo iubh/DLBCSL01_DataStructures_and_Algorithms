bd1372f1d87b08c88534f489967555a724f94317
Paul Libbrecht <paul.libbrecht@cabri.com>
2022-10-23 00:27:42 +0200

    clarification

--- Unit1_Basic_Concepts/dList.py
+++ Unit1_Basic_Concepts/dList.py
@@ -16,50 +16,61 @@ class DNode:
     def setNextNode(self, elem):
         self.nextNode = elem
 
-anode = DNode(22)
-print(anode.getElement())
-
 
 class DoublyLinkedList:
     def __init__(self):
         self.length = 0
         self.head = DNode(None)
         self.tail = DNode(None)
-    
+
     def isEmpty(self):
         return (self.length==0)
-    
+
     def getLength(self):
         return self.length
-   
-    
+
+
     def _addNodeIntermediate(self, elem, prev, next):
         #Add element between nodes prev and nextelem
-        temp = DNode(elem, prev, next)
-        prev.setNextNode(temp)
-        next.setPrevNode(temp)
+        if self.length == 0:
+            node = DNode(elem, self.head, self.tail)
+            self.head.nextNode = node
+            self.tail.prevNode = node
+        else:
+            temp = DNode(elem, prev, next)
+            prev.setNextNode(temp)
+            next.setPrevNode(temp)
         self.length +=1
-    
+
     def _deleteNodeIntermediate(self, elem):
         #Remove intermediate node from list
         lastNode = elem.getPrevNode()
         nextNode = elem.getNextNode()
         lastNode.setNextNode(nextNode)
         nextNode.setPrevNode(lastNode)
-        self.length -=1    
-   
+        self.length -=1
+
     def addNodeFront(self,elem):
        #Add element immediately after head node
-       self._addNodeIntermediate(self, elem, self.head, self.head.nextNode)
-       
+       self._addNodeIntermediate(elem, self.head, self.head.nextNode)
+
     def addNodeEnd(self,elem):
-       #Add element immediately after head node
-       self._addNodeIntermediate(self, elem, self.tail.prevNode, self.tail)   
-          
-    
-        
-       
-    
-    
-        
+       #Add element immediately before tail node
+       self._addNodeIntermediate(elem, self.tail.prevNode, self.tail)
+
+
+
+
+
+
+
+list = DoublyLinkedList()
+list.addNodeEnd("33")
+list.addNodeFront("1")
+list.addNodeEnd("44")
+node = list.head
+while ( node != list.tail):
+    print(node.element)
+    node = node.nextNode
+
 
